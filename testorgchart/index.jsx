c58c77be62a83ad031a50a71919b62b5e6513303
ZornitsaPesheva <pesheva@gmail.com>
2026-04-17 17:49:37 +0300

    това също работи локално

--- testorgchart/index.jsx
+++ testorgchart/index.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { OrgChartReact, Employee } from 'balkan-orgchart-react';
+import { OrgChartReact, Employee } from "balkan-orgchart-react";
 
-export const ChartExample = () => {
-  return   <OrgChartReact nodeBinding={{ field_0: 'name', field_1: 'title' }} style={{width: '100%', height: '100%', backgroundColor: 'lightblue'}} enableSearch={false}>
-      <Employee name='Denny Curtis' title='CEO'>
-        <Employee name='Lexie Cole' title='QA Lead'></Employee>
-        <Employee name='Janae Barrett' title='Technical Director'></Employee>
-        <Employee name='Aaliyah Webb' title='Assistant' assistant={true}></Employee>
-      </Employee>
-     </OrgChartReact>
+function ChartExample() {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <OrgChartReact
+        nodeBinding={{ field_0: "name", field_1: "title" }}
+        style={{ width: "100%", height: "100%", backgroundColor: "lightblue" }}
+        enableSearch={false}
+      >
+        <Employee name="Denny Curtis" title="CEO">
+          <Employee name="Lexie Cole" title="QA Lead" />
+          <Employee name="Janae Barrett" title="Technical Director" />
+          <Employee name="Aaliyah Webb" title="Assistant" assistant={true} />
+        </Employee>
+      </OrgChartReact>
+    </div>
+  );
 }
 
 createRoot(document.getElementById("root")).render(
-  React.createElement(
-    React.StrictMode,
-    null,
-    React.createElement(ChartExample, null)
-  )
+  <React.StrictMode>
+    <ChartExample />
+  </React.StrictMode>
 );
