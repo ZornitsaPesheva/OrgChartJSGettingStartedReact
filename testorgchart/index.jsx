275ff3fadcbacfcfbb656b29506607f29af4d342
Zornitsa Serbezova <zornitsa.pesheva@outlook.com>
2026-04-17 12:48:04 +0300

    тест симпъл jsx

--- testorgchart/index.jsx
+++ testorgchart/index.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-function App() {
-  return <h1>Hello, React + Vite!</h1>;
-}
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <h1>Hello React 👋</h1>
   </React.StrictMode>
 );
